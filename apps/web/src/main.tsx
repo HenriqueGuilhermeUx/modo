cc86a06e5ec5819f0902d3bdd09e8aba1980a05b
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-19 21:11:04 -0300

    boot landing and account portal

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Root from "./Root";
 import "./styles.css";
 import "./pricing.css";
+import "./portal.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Root />
+  </StrictMode>,
+);
